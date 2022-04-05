15bc844024fccb6f67fb02abd215aaa705567a2f
Victor Dimitrov Petrov <vdpetrov2019@pgaz.org>
2022-04-05 23:18:24 +0300

    Fixed Explore page (Km/h to Knots) converter.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ function kmkts() {
 
     if (result >= 0) {
         document.getElementById("resultOutput").innerHTML = result;
+        
+        if (result >= 666.7389036) {
+            document.getElementById("resultOutput").innerHTML = result.toFixed(2);
+            alert("Speed of sound reached/exceeded!");
+        }
     }
     else {
         alert("Please enter valid number!");
     }
+
 }
 
 document.getElementsByClassName("wrapper").style.height = "200px";
